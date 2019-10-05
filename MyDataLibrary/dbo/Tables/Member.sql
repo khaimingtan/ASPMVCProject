abc70274b25f6628ff2723c28d5bfd7082ea9116
Khai Ming Tan <SpideyMing@hotmail.com>
2019-10-05 10:45:14 +0800

    Push

---
+++ MyDataLibrary/dbo/Tables/Member.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Member]
+(
+	[LoginId] NVARCHAR(16) NOT NULL PRIMARY KEY, 
+    [MemberRole] INT NULL, 
+    [FirstName] NVARCHAR(50) NULL, 
+    [LastName] NVARCHAR(50) NULL, 
+    [Email] NVARCHAR(50) NULL, 
+    [Password] NVARCHAR(20) NULL
+)
---
+++ MyDataLibrary/dbo/Tables/Product.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Product]
+(
+	[ProductId] NVARCHAR(20) NOT NULL PRIMARY KEY, 
+    [ProductName] NCHAR(100) NULL, 
+    [StockQty] INT NULL, 
+    [UnitPrice] REAL NULL
+)
